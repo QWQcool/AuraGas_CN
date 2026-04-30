eecf624e6b848f70c8e354bbb9767ac0e4d3679b
v_chchsli <v_chchsli@tencent.com>
2026-04-30 17:27:43 +0800

    feat: 增强聊天历史和IDE配置，新增多字段与组件

--- Source/AuraGas/Public/Actor/AuraEffectActor.h
+++ Source/AuraGas/Public/Actor/AuraEffectActor.h
@@ -7,6 +7,21 @@
 
 #include "AuraEffectActor.generated.h"
 
+UENUM(BlueprintType)
+enum class EEffectApplicationPolicy : uint8
+{
+	ApplyOnOverlap,
+	ApplyOnEndOverlap,
+	DoNotApply
+};
+
+UENUM(BlueprintType)
+enum class EEffectRemovePolicy : uint8
+{
+	RemoveOnEndOverlap,
+	DoNotRemove
+};
+
 
 UCLASS()
 class AURAGAS_API AAuraEffectActor : public AActor
@@ -26,8 +41,36 @@ protected:
 	UFUNCTION(BlueprintCallable)
 	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 
+	UFUNCTION(BlueprintCallable)
+	void OnOverlap(AActor* TargetActor);
+
+	UFUNCTION(BlueprintCallable)
+	void OnEndOverlap(AActor* TargetActor);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly,Category = "Applied Effects")
+	bool bDestroyOnEffectRemove = false;
+	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly,Category = "Applied Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly,Category = "Applied Effects")
+	EEffectApplicationPolicy InstantEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly,Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly,Category = "Applied Effects")
+	EEffectApplicationPolicy DurationEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly,Category = "Applied Effects")
+	TSubclassOf<UGameplayEffect> InfiniteGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly,Category = "Applied Effects")
+	EEffectApplicationPolicy InfiniteEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly,Category = "Applied Effects")
+	EEffectRemovePolicy EffectRemovePolicy = EEffectRemovePolicy::RemoveOnEndOverlap;
 private:
 
 };
